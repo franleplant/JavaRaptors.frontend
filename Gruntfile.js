d31340c1d4356c9e12c4e0651b8989041f69be15
ext.fguijarro <f.guijarro@globant.com>
2013-08-19 11:15:23 -0300

    jshint config

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,42 @@ module.exports = function(grunt) {
 		jshint: {
 			files: ['js/*.js', 'gruntfile.js'],
 			options: {
+				// check http://www.jshint.com/docs/options/#bitwise
+				bitwise: true,
+				camelcase: false,
+				curly: true,
+				eqeqeq: true,
+				es3: false,
+				forin: true,
+				immed: true,
+				indent: 4,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				noempty: true,
+				nonew: false,
+				plusplus: false,
+				quotmark: true,
+				undef: true,
+				unused: 'strict',
+				strict: false,
+				trailing: true,
+				maxparams: false,
+				maxdepth: false,
+				maxstatements: false,
+				maxcomplexity: false,
+				maxlen: false,
+				browser: true,
+				devel: true,
+				
 
+				// Global Variables permitted
+				globals: {
+					module: true,
+					jQuery: true,
+					$: true,
+					'las de angular': true
+				}
 			}
 		},
 		karma: {
@@ -25,7 +60,7 @@ module.exports = function(grunt) {
 			all: {
 				files: ['<%= jshint.files %>'],
 				tasks: ['default']
-			}		
+			}
 		}
 	});
 
@@ -34,4 +69,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-karma');
 
 	grunt.registerTask('default', ['jshint', 'karma']);
+
 };
