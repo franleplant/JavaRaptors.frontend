c923c59f3bbd112080c1d26c9357af12ece9f07d
ext.fguijarro <f.guijarro@globant.com>
2013-08-24 21:06:33 -0300

    trying to get a minimun functional markup and css

---
+++ src/js/animations.js
@@ -0,0 +1,14 @@
+(function ($) {
+
+
+	// Search panel going up panel
+	// this should be a custom event named "first valid search"
+	$('#search').click(function () {
+		$("#search_group").animate({'marginTop': '0px'}, 500);
+	});
+
+}) (jQuery);
+
+
+
+
