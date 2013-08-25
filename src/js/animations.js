9607c58d39f1bb46a9fbfe5c8cd38a7384aa9886
ext.fguijarro <f.guijarro@globant.com>
2013-08-24 21:11:57 -0300

    index.html is on its ideal starting point

--- src/js/animations.js
+++ src/js/animations.js
@@ -4,6 +4,8 @@
 	// Search panel going up panel
 	// this should be a custom event named "first valid search"
 	$('#search').click(function () {
+
+
 		$("#search_group").animate({'marginTop': '0px'}, 500);
 	});
 
