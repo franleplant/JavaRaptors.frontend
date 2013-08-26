f44870ef313c65c98020a1a566a4ba4edebc74fe
ext.fguijarro <f.guijarro@globant.com>
2013-08-26 13:44:47 -0300

    ux search query onclik ++

---
+++ src/js/animationsux.js
@@ -0,0 +1,25 @@
+// Animations and UX
+// Put all the js code regarding animations, and user experience, 
+// you will be propably using jquery, put everything inside the anonymous function
+
+
+(function ($) {
+
+	// On page load focus on the search query
+	$('#search_query').focus();
+
+	// When you click on the search query select all the text
+	$('#search_query').on('click', function () {
+		$(this).select();
+	});
+
+	// Search panel going up panel
+	jraptors.animations.on('search.first_valid', function () {
+		$('#search_group').animate({'marginTop': '0px'}, 500);
+	});
+
+}) (jQuery);
+
+
+
+
