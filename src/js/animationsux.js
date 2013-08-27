f8126f6c243cb18448053eccbb513a83ed539477
franleplant <f.guijarro@globant.com>
2013-08-27 15:45:48 -0300

    working and testing things with the form

--- src/js/animationsux.js
+++ src/js/animationsux.js
@@ -13,6 +13,7 @@
 		$(this).select();
 	});
 
+
 	// Search panel going up panel
 	jraptors.animations.on('search.first_valid', function () {
 		$('#search_group').animate({'marginTop': '0px'}, 500);
