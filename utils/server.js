7fd33dc239c43e41db93023df2f0dccd19d121ec
ext.fguijarro <f.guijarro@globant.com>
2013-08-29 16:19:11 -0300

    remove unnecesary server file

--- utils/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-app.configure(function () {
-    app.use(
-        "/", //the URL throught which you want to access to you static content
-        express.static('../../') //where your static content is located in your filesystem
-    );
-});
-app.listen(3000); //the port you want to use
