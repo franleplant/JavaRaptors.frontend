41bc981b2733d2b010187755d34152681f4a9ff9
franleplant <franleplant@gmail.com>
2013-09-09 14:49:09 -0300

    first e2e test running OK

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing 
 
 describe('my app', function() {
 
@@ -42,4 +42,18 @@ describe('my app', function() {
     });
 
   });
+});
+
+*/
+
+describe('jraptors', function () {
+
+    beforeEach(function() {
+        browser().navigateTo('../../app/index.html');
+    });
+
+    it('should automatically redirect to /book when location hash/fragment is empty', function() {
+        expect(browser().location().url()).toBe("/book");
+    });
+
 });
