b385e016565dc1a120a5e3d88d6622d3f70b651d
franleplant <franleplant@gmail.com>
2013-09-10 15:08:44 -0300

    +e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -92,15 +92,22 @@ describe('jraptors', function () {
 
 		describe('book search results (with results)', function () {
 
-
-			it('should render book_snippet template whenever a search is done', function () {
-
+			beforeEach(function() {
 				input('search_query').enter('Harry Potter');
-
 				element('#search').click();
-
-				expect(  repeater('#results > div.result').count()  ).toBeGreaterThan(1);				
 			});
+
+
+			it('should render book search results', function () {
+				expect(  repeater('div.result').count()  ).toBeGreaterThan(1);				
+			});
+
+			it('should render book_snippet template', function () {
+				expect(  repeater('div.result:first > div > span').count()  ).toBe(1);
+				expect(  repeater('div.result:first > div > a').count()  ).toBe(1);	
+				expect(  repeater('div.result:first > div > div.media-body').count()  ).toBe(1);				
+			});
+
 
 
 		});
