61f608f6af4c3a0652349005957d8fd82f1089b4
franleplant <franleplant@gmail.com>
2013-09-11 17:33:32 -0300

    book/create view++

--- app/scripts/controller/controllers.js
+++ app/scripts/controller/controllers.js
@@ -77,6 +77,9 @@ controller('LendController',
 	]
 ).
 
+
+
+
 controller('CreateController',
 	[
 		'$scope',
@@ -84,6 +87,10 @@ controller('CreateController',
 		}
 	]
 ).
+
+
+
+
 
 controller('NavBarController',
 	[
