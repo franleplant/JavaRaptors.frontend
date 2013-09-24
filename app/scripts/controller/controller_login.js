65de317e4165edd53381a761a519dfcecef958df
franleplant <franleplant@gmail.com>
2013-09-24 12:37:36 -0300

    add error alerts

--- app/scripts/controller/controller_login.js
+++ app/scripts/controller/controller_login.js
@@ -4,6 +4,11 @@ controller('SigninController', ['$scope', '$http',
 	
 		function ($scope, $http) {
 
+			$scope.validation = {
+				userName: true,
+				password: true
+			};
+
 			$scope.signin = function () {
 
 				$http({
@@ -13,12 +18,20 @@ controller('SigninController', ['$scope', '$http',
 
 					success(function(data, status, headers, config) {
 						console.log('success', data);
+
+						if (data.userName && data.password) {
+							//location.href = 'index.html';
+							return;		
+						}
+
+						$scope.validation.userName = data.userName;
+						$scope.validation.password = data.password;
 					}).
 					error(function(data, status, headers, config) {
 						console.log('error', data);
 					});
 
-				//location.href = 'index.html';
+				
 
 			};
 
