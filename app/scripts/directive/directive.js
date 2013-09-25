cf0a6a1ff1c6424d0801a808c7e063ee9ad0335a
franleplant <franleplant@gmail.com>
2013-09-25 00:13:52 -0300

    contentEditable is working great now exclamation mark

--- app/scripts/directive/directive.js
+++ app/scripts/directive/directive.js
@@ -1,18 +1,40 @@
 angular.module('jraptorsDirectives', []).
 
-	// Directive to hide elements according to user Role
-	directive('restrictToRole',  [ 'UserSession',
-			function (UserSession) {
+// Directive to hide elements according to user Role
+directive('restrictToRole',  [ 'UserSession',
+		function (UserSession) {
 
-				function link(scope, element, attrs) {
-					if (  UserSession.role()  !== attrs.restrictToRole) {
-						element.hide();
-					}
+			function link(scope, element, attrs) {
+				if (  UserSession.role()  !== attrs.restrictToRole) {
+					element.hide();
 				}
-
-				return {
-					link: link
-				};
 			}
-		]
-	);
+
+			return {
+				link: link
+			};
+		}
+	]
+).
+
+//http://docs.angularjs.org/guide/forms
+directive('contenteditable', function() {
+	return {
+		require: 'ngModel',
+		link: function(scope, elm, attrs, ctrl) {
+			// view -> model
+			elm.on('blur', function() {
+				console.log(scope)
+				scope.$apply(function() {
+					ctrl.$setViewValue(elm.html());
+				});
+			});
+
+			// model -> view
+			ctrl.$render = function() {
+				elm.html(ctrl.$viewValue);
+			};
+
+		}
+	};
+});
